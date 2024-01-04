f0d14ec45952fd18116029c6b4cbc9abc2519511
ziyu <tehziyu@chsbp.edu.my>
2024-01-04 14:43:17 +0800

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 let tl = gsap.timeline();
-gsap.defaults({ ease: "power4.out" });
+gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ duration: 1, ease: "power4.out" });
+
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
@@ -32,11 +45,12 @@ tl.to(
     "<"
   )
   .to(
-    ".banner",
+    "#banner",
     { duration: 1, opacity: "1", ease: "expo.out", y: "0%" },
-    "-=0.5"
+    "-=0.75"
   )
-  .to(".navbutton", { opacity: 1, y: 0 }, "-=0.5");
+  .to(".navbutton", { opacity: 1, y: 0 }, "<");
+
 let clickCount = 0;
 function navClick() {
   clickCount++;
@@ -89,3 +103,26 @@ function navClose() {
     backgroundColor: "#333",
   });
 }
+
+gsap.to(".element", {
+  x: "0%",
+  opacity: 1,
+  scrollTrigger: {
+    start: "top 40%",
+    end: "top 20%",
+    markers: true,
+    trigger: ".element",
+    //scrub: true,
+    toggleAction: "restart none none none",
+  },
+});
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
