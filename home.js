b94d07c5d6b229831cfc68a70f2e2417d558ea0c
ziyu <tehziyu@chsbp.edu.my>
2024-01-06 11:20:53 +0800

    minor changes

--- home.js
+++ home.js
@@ -9,13 +9,6 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-const swiper = new Swiper(".swiper", {
-  spaceBetween: 30,
-  /*   autoplay: {
-    delay: 2000,
-  }, */
-});
-
 let tl = gsap.timeline();
 gsap.registerPlugin(ScrollTrigger);
 gsap.defaults({ duration: 1, ease: "power4.out" });
@@ -25,16 +18,32 @@ window.onbeforeunload = function () {
   document.documentElement.scrollTop = 0;
 };
 
-tl.to("#banner", { duration: 1, opacity: "1", ease: "expo.out", y: "0%" }).to(
-  ".bar",
-  {
-    duration: 0.75,
-    opacity: 1,
-    y: 0,
-    stagger: 0.1,
-  },
-  "<"
-);
+tl.to("#banner", {
+  duration: 1,
+  opacity: "1",
+  ease: "expo.out",
+  y: "0%",
+})
+  .to(
+    ".bar",
+    {
+      duration: 0.75,
+      opacity: 1,
+      y: 0,
+      stagger: 0.1,
+    },
+    "<"
+  )
+  .to(
+    "#banner",
+    {
+      duration: 1,
+      opacity: 0,
+      y: "-100%",
+      display: "none",
+    },
+    "+=0.5"
+  );
 
 let clickCount = 0;
 function navClick() {
@@ -88,50 +97,3 @@ function navClose() {
     backgroundColor: "#333",
   });
 }
-
-gsap.to(".element", {
-  x: "0%",
-  opacity: 1,
-  duration: 0.5,
-  scrollTrigger: {
-    start: "top 40%",
-    end: "top 10%",
-    trigger: "#page2",
-    toggleActions: "restart reverse restart reverse",
-  },
-});
-gsap.to(".swipe-hint", {
-  scrollTrigger: {
-    trigger: ".swiper",
-    start: "20% bottom",
-    end: "80% center",
-    toggleActions: "restart restart restart none",
-  },
-  opacity: 0,
-  repeat: 2,
-  yoyo: true,
-  ease: "none",
-  duration: 1,
-});
-gsap.to(".swiper", {
-  scrollTrigger: {
-    trigger: "#swiper-container",
-    start: "top bottom",
-    end: "top 20%",
-    scrub: true,
-  },
-  scaleX: 1,
-  opacity: 1,
-  ease: "none",
-  duration: 1,
-});
-let viewButtonAnimation = gsap.to(".view-button-container", {
-  paused: true,
-  duration: 0.25,
-  ease: "power1.inOut",
-  scale: 1.1,
-});
-let viewButton = document.querySelector(".view-button-container");
-
-viewButton.addEventListener("mouseenter", () => viewButtonAnimation.play());
-viewButton.addEventListener("mouseleave", () => viewButtonAnimation.reverse());
